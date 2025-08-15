8b01eb982f1681722751025251231c37dd44ed18
reverseon <reverseon@outlook.com>
2025-08-15 13:25:17 +0000

    remove tags

--- aws/general-purpose/ecr.tf
+++ aws/general-purpose/ecr.tf
@@ -1,10 +1,4 @@
 # Public ECR Repository
 resource "aws_ecrpublic_repository" "main" {
   repository_name = "ishiori-k8s-dex-client"
-
-  tags = {
-    Name        = "ishiori-k8s-dex-client"
-    Environment = "production"
-    Project     = "ishiori"
-  }
 }
