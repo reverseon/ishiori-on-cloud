77cf27ee1554c33ea56fd411217de8537d35a3ce
reverseon <reverseon@outlook.com>
2025-08-16 13:32:36 +0000

    Hotfix #2

--- aws/general-purpose/iam.tf
+++ aws/general-purpose/iam.tf
@@ -5,6 +5,13 @@ resource "aws_iam_role" "ecr_pull_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::319844025384:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_9ac8c34c1d1b91bb"
+        }
+        Action = "sts:AssumeRole"
+      }
     ]
   })
 
