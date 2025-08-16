0e94caae78e8e2552ac602446e42f2ab4fee6cf9
reverseon <reverseon@outlook.com>
2025-08-16 13:16:10 +0000

    Revamp ECR

--- aws/general-purpose/ecr.tf
+++ aws/general-purpose/ecr.tf
@@ -3,26 +3,3 @@ resource "aws_ecr_repository" "main" {
   name                 = "ishiori-k8s-private-ecr"
   image_tag_mutability = "MUTABLE"
 }
-
-# ECR Repository Policy - allows pull access only to the ECR role
-resource "aws_ecr_repository_policy" "main" {
-  repository = aws_ecr_repository.main.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowPullFromECRRole"
-        Effect = "Allow"
-        Principal = {
-          AWS = aws_iam_role.ecr_full_access_role.arn
-        }
-        Action = [
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "ecr:BatchCheckLayerAvailability"
-        ]
-      }
-    ]
-  })
-}
