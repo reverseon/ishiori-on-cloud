84f8ee68660e2c1688868ddd94cc36eca19ce8b9
reverseon <reverseon@outlook.com>
2025-08-19 16:25:26 +0000

    Add Lifecycle

--- aws/general-purpose/ecr.tf
+++ aws/general-purpose/ecr.tf
@@ -3,3 +3,26 @@ resource "aws_ecr_repository" "main" {
   name                 = "ishiori-k8s-private-ecr"
   image_tag_mutability = "MUTABLE"
 }
+
+# ECR Lifecycle Policy
+resource "aws_ecr_lifecycle_policy" "main" {
+  repository = aws_ecr_repository.main.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep only 2 latest images for k8s-dex-client_* tags"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["k8s-dex-client_"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 2
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
