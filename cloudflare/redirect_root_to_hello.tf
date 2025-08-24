8da3d99513ae93a4f68ab6068bc7dff6109b32d5
reverseon <reverseon@outlook.com>
2025-08-24 16:44:36 +0000

    use cloudflare_page_rule

--- cloudflare/redirect_root_to_hello.tf
+++ cloudflare/redirect_root_to_hello.tf
@@ -1,20 +1,10 @@
-resource "cloudflare_ruleset" "redirect_root_to_hello" {
-  zone_id     = cloudflare_zone.ishiori_net.id
-  name        = "Redirect root to hello.ishiori.net"
-  description = "Redirect ishiori.net to hello.ishiori.net using Cloudflare Ruleset"
-  kind        = "zone"
-  phase       = "http_request_redirect"
-
-  rules = [
-    {
-      action = "redirect"
-      action_parameters = {
-        status_code = 301
-        url         = "https://hello.ishiori.net/$uri$?$query$"
-      }
-      expression  = "http.host eq 'ishiori.net'"
-      description = "Redirect root domain to hello.ishiori.net"
-      enabled     = true
+resource "cloudflare_page_rule" "redirect_root_to_hello" {
+  zone_id  = cloudflare_zone.ishiori_net.id
+  target   = "ishiori.net/*"
+  actions = {
+    forwarding_url = {
+      url         = "https://hello.ishiori.net/$1"
+      status_code = 301
     }
-  ]
+  }
 }
